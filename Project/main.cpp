094ecd60480b89d2c157805e5cbb62fdfcf1c74f
Yujin Jang <jyooj08@gmail.com>
2020-05-20 23:37:19 +0900

    Create Logic class

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,13 +2,50 @@
 #include"cgut.h"
 #include<iostream>
 using namespace std;
-#define TIME_DELTA 1
+#define TIME_DELTA 0.5
 
 ivec2 window_size = cg_default_window_size();
-char current_move;
-//up -> right -> down -> left
-char direction[] = { 'u','r','d','l' };
-int di = 0;
+
+class Logic {
+public:
+	char direction[4];
+	int di = 0, mi = 0;
+	int game_mode = 0; // 0: before start, 1: during game, 2: win game, 3: lose game
+	string map;
+	int map_len;
+	double last_time, now;
+
+	Logic(string m) {
+		direction[0] = 'u'; direction[1] = 'r'; direction[2] = 'd'; direction[3] = 'l';
+		map = m;
+		map_len = map.length();
+	}
+
+	void update() {
+		if (game_mode == 0) return;
+		if (game_mode == 2) {
+			cout << "WIN GAME" << endl;
+			game_mode = 0;
+			return;
+		}
+		if (game_mode == 3) {
+			cout << "LOSE GAME" << endl;
+			game_mode = 0;
+			return;
+		}
+
+		now = glfwGetTime();
+		if (mi == map_len) game_mode = 2;
+		else if (mi < map_len && now - last_time >= TIME_DELTA) {
+			cout << "current map: " << map[mi] << " current_move: " << direction[di] << endl;
+			if (map[mi] != direction[di]) game_mode = 3;
+			mi++;
+			last_time = now;
+		}
+	}
+};
+
+Logic logic("uuuurruu");
 
 void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods);
 void reshape(GLFWwindow* window, int width, int height);
@@ -28,29 +65,15 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, reshape);
 	glfwSetKeyCallback(window, keyboard);
 
-	string map = "uuuurruu";
-	double last_time = glfwGetTime();
-	int idx = 0;
-	int map_len = map.length();
-	current_move = map[0];
-
-	cout << "map: " << map << endl;
+	// logic setting
+	logic.last_time = glfwGetTime();
+	cout << "map: " << logic.map << endl;
 	cout << "TIME_DELTA: " << TIME_DELTA << "s" << endl << endl;
+	logic.game_mode = 1;
 
 	while (!glfwWindowShouldClose(window))
 	{
-		double now = glfwGetTime();
-		current_move = direction[di];
-		//cout << "now: " << now << endl;
-		if (idx < map_len && now - last_time >= TIME_DELTA) {
-			cout << "current map: " << map[idx] << " current_move: " << current_move << endl;
-			if (map[idx] != current_move) {
-				cout << "Lose Game...." << endl;
-				idx = map_len;
-			}
-			idx++;
-			last_time = now;
-		}
+		logic.update();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -70,14 +93,14 @@ void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
 		if (key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q)
 			glfwSetWindowShouldClose(window, true);
 		else if (key == GLFW_KEY_RIGHT) {
-			di++;
-			if (di > 3) di = 0;
-			cout << "direction change: " << direction[di] << endl;
+			logic.di++;
+			if (logic.di > 3) logic.di = 0;
+			cout << "Turn right" << endl;
 		}
 		else if (key == GLFW_KEY_LEFT) {
-			di--;
-			if (di < 0) di = 3;
-			cout << "direction change: " << direction[di] << endl;
+			logic.di--;
+			if (logic.di < 0) logic.di = 3;
+			cout << "Turn left" << endl;
 		}
 	}
 }
